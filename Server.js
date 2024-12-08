5cde79ce28c6fd6db110b219ee33de25a896b05e
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-07 21:32:30 -0500

    doc: added documentation  for Express server setup and route handling

--- Server.js
+++ Server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-
+/**
+ * Route handler for the root URL ('/').
+ * Responds with a 'hello world' message.
+ */
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
 const port = 5000;
+/**
+ * Start the server and listen on the specified port.
+ * Logs a message to the console when the server is running.
+ */
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
